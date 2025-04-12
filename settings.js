e6538783dfae52483cbc03f6d999cc77c3585892
CENNCH <odigiealexeghosa1@gmail.com>
2025-04-12 12:46:41 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Cench's bot', // Your name
-  ownerNumber: '+2348140067102', //Your number
+  botOwner: 'Cench bot', // Your name
+  ownerNumber: '+8140067102', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
